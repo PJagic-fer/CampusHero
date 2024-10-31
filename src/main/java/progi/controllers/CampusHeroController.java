453e9698bd7a1f6986c835efab3dd32345ea8336
Ana Heski <ana.heski@unizg.fer.hr>
2024-10-31 16:28:54 +0100

    dodan Controller za početnu stranicu

---
+++ src/main/java/progi/controllers/CampusHeroController.java
@@ -0,0 +1,20 @@
+package progi.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+
+@RestController
+public class CampusHeroController {
+
+    @GetMapping("/")
+    public RedirectView redirectToCampusHero(){
+        return  new RedirectView("/campus_hero");
+    }
+
+    @GetMapping("/campus_hero")
+    public String getCampusHero(){
+        return ("Dobrodošli na CampusHero");
+    }
+}
