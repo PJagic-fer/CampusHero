54ab0a2b145082415a176d5b966769f4fab873a2
Ana Heski <ana.heski@unizg.fer.hr>
2024-11-03 20:00:06 +0100

    Kreirana osnovna baza (memorijska H2)

---
+++ src/main/java/progi/data/Busyness.java
@@ -0,0 +1,59 @@
+package progi.data;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+
+@Entity
+public class Busyness {
+    @Id
+    @SequenceGenerator(
+            name = "busy_sequence",
+            sequenceName = "busy_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "busy_sequence"
+    )
+    private Long id;
+
+    @ManyToOne
+    private Canteen canteen;
+
+    private Integer score;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime time;
+
+    public Busyness(Canteen canteen, Integer score) {
+        this.canteen = canteen;
+        this.score = score;
+        this.time = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Canteen getCanteen() {
+        return canteen;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public LocalDateTime getTime() {
+        return time;
+    }
+
+    public void setCanteen(Canteen canteen) {
+        this.canteen = canteen;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+}
