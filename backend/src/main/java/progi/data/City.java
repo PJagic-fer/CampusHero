13b568b252009a3b7024035f83f708a6e3a3a785
Ana Heski <ana.heski@unizg.fer.hr>
2024-11-11 22:25:21 +0100

    poštanski broj zamijenjen id-jem

--- backend/src/main/java/progi/data/City.java
+++ backend/src/main/java/progi/data/City.java
@@ -6,29 +6,22 @@ import jakarta.persistence.*;
 public class City {
 
     @Id
-    @SequenceGenerator(
-            name = "city_sequence",
-            sequenceName = "city_sequence",
-            allocationSize = 1
-    )
-    @GeneratedValue(
-            strategy = GenerationType.SEQUENCE,
-            generator = "city_sequence"
-    )
+    @SequenceGenerator(name = "city_sequence", sequenceName = "city_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "city_sequence")
     @Column(unique = true)
-    private Integer postal_code;
+    private Integer id;
 
     private String name;
 
-    public City() {}
+    public City() {
+    }
 
-    public City(Integer postal_code, String name) {
-        this.postal_code = postal_code;
+    public City(String name) {
         this.name = name;
     }
 
-    public Integer getPostal_code() {
-        return postal_code;
+    public Integer getId() {
+        return id;
     }
 
     public String getName() {
