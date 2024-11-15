aca7a3f46a34687d209637e9185dcba56dcf3c24
pm5323 <pm53233@fer.hr>
2024-11-15 19:26:28 +0100

    Undo promjene

--- backend/src/main/java/progi/config/CorsConfig.java
+++ backend/src/main/java/progi/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Allow all endpoints
-                .allowedOrigins("https://frontend-latest-omcn.onrender.com", "https://frontend-latest-omcn.onrender.com") // Allow all origins
+                .allowedOrigins("http://localhost", "https://frontend-latest-omcn.onrender.com") // Allow all origins
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow specific HTTP methods
                 .allowedHeaders("*") // Allow all headers
                 .allowCredentials(true); // Allow credentials
