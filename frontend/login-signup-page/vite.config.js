5c3a73deb0b5e0852d668f7733e8ffa630d7b0cf
pm5323 <pm53233@fer.hr>
2024-12-23 14:38:26 +0100

    Frontend i backend povlace portove iz env

--- frontend/login-signup-page/vite.config.js
+++ frontend/login-signup-page/vite.config.js
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 80,
+export default defineConfig(({mode}) =>{
+  const env = loadEnv(mode, process.cwd());
+
+  const PORT = `${env.FRONTEND_PORT ?? 80}` // ili port iz env ili 80
+
+  return {
+    plugins: [react()],
+    server: {
+      port: PORT,
+    }
   }
 })
