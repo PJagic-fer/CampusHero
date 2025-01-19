1912972d8f0f6539e7692bcc4596aca8e33564c1
Ana Heski <ana.heski@fer.unizg.hr>
2025-01-19 22:46:53 +0100

    Dodano dojavljivanje gužve u menzi na backend Ostvareno dodavanje novih i dohvaćanje filtiriranih informacija o gužvi Obrisan višak iz kontrolera

--- backend/src/main/java/progi/repositories/BusynessRepository.java
+++ backend/src/main/java/progi/repositories/BusynessRepository.java
@@ -1,8 +1,14 @@
 package progi.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import progi.data.Busyness;
+import progi.data.Canteen;
 
 @Repository
-public interface BusynessRepository extends JpaRepository<Busyness, Long> {}
+public interface BusynessRepository extends JpaRepository<Busyness, Long> {
+    public List<Busyness> findByCanteen(Canteen canteen);
+}
